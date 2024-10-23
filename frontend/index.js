bd792419ed9359e2e84814afa6029c508eafdc87
Conner Krenzer <krenzer84@gmail.com>
2024-10-23 16:59:18 -0400

    done

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,16 @@
-import './styles/reset.css'
-import './styles/styles.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
+import "./styles/reset.css";
+import "./styles/styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+import { TodosProvider } from "./context/todos";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 root.render(
-  <App />
-)
+  <TodosProvider>
+    <App />
+  </TodosProvider>
+);
